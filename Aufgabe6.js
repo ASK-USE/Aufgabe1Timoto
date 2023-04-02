6d3c7923b89df73a2511ef1e04c9dc41c3729f83
timotoo <124346112+timotoo@users.noreply.github.com>
2023-04-02 19:04:40 +0200

    Create Aufgabe6.js

---
+++ Aufgabe6.js
@@ -0,0 +1,7 @@
+let Number1 = promp(Please enter first number:");
+let Number2 = prompt("Please enter second number:);
+
+let sum - Number1+number2
+
+      console.loge(sum);
+console.log(typpeoff sum);
