1235d1202842800191720a02ad75a218fd04e8d3
ASK-USE <knueppelalex@web.de>
2023-04-02 19:28:44 +0200

    fehler viele

--- Aufgabe6.js
+++ Aufgabe6.js
@@ -1,7 +1,7 @@
-let Number1 = promp(Please enter first number:");
-let Number2 = prompt("Please enter second number:);
+let Number1 = prompt("Please enter first number:");
+let Number2 = prompt("Please enter second number:");
 
-let sum - Number1+number2
+let = sum = Number1 + number2
 
-      console.loge(sum);
-console.log(typpeoff sum);
+      console.log(sum);
+console.log(typeoff= sum);
